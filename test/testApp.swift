41ca1e5a9c48bb6cc44f0c49114c6bf29fbd7fc0
Olivia <candy930420@gmail.com>
2024-07-30 20:14:15 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Olivia Chen on 2024/7/30.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
